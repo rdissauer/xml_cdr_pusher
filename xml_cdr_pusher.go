8c4dbc87c2214701ccec1a04c528e372a62a89a8
rdissauer <roman@dissauer.net>
2016-07-11 07:53:31 +0200

    added failure handling added xml to json conversion fixed insert statement

--- xml_cdr_pusher.go
+++ xml_cdr_pusher.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
+	xj "github.com/basgys/goxml2json"
 
 	_ "github.com/lib/pq"
 )
@@ -39,12 +41,22 @@ func (t *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		BlegUUID                string   `xml:"callflow>caller_profile>origination>origination_caller_profile>uuid"`
 	}
 
-	path := r.URL.Path[1:]
-	log.Println(path)
+	//path := r.URL.Path[1:]
+	//log.Println(path)
 
 	body, _ := ioutil.ReadAll(r.Body)
 	decbody, _ := url.QueryUnescape(string(body))
 	//log.Println(decbody)
+	xmlraw := decbody	
+	xmlreader := strings.NewReader(xmlraw)
+	json, jerr := xj.Convert(xmlreader)
+	if jerr != nil {
+		w.WriteHeader(500)
+                w.Write([]byte("500 - " + http.StatusText(503) + "\nJSON conversion failed!"))
+		log.Println("JSON conversion failed!")
+		return
+	}
+	//log.Println(json.String())
 
 	//decoder := xml.NewDecoder(r.Body)
 	res := &Result{}
@@ -52,32 +64,51 @@ func (t *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	err := xml.Unmarshal([]byte(decbody), res)
 
+	res.StartStamp, _ = url.QueryUnescape(res.StartStamp)
+	res.AnswerStamp, _ = url.QueryUnescape(res.AnswerStamp)
+	res.EndStamp, _ = url.QueryUnescape(res.EndStamp)
+
 	//log.Println(err)
-	log.Println(*res)
+	//log.Println(*res)
 
-	db, dberr := sql.Open("postgres", "user=test dbname=test password=qakyrGVgyh3Q7ZWwyEgj sslmode=verify-full")
+	db, dberr := sql.Open("postgres", "user=test dbname=test password=qakyrGVgyh3Q7ZWwyEgj sslmode=require")
 	if dberr != nil {
+		w.WriteHeader(500)
+                w.Write([]byte("500 - " + http.StatusText(503) + "\nFailed to connect to DB!"))
 		log.Fatal(dberr)
+		return
 	}
+	defer db.Close()
 
-	qerr := db.QueryRow(`INSERT INTO xml_cdr (direction, caller_id_name, caller_id_number, called_destination_number, destination_number, context,
+	_, qerr := db.Query(`INSERT INTO xml_cdr (direction, caller_id_name, caller_id_number, called_destination_number, destination_number, context,
 		                                        core_uuid, switchname, start_stamp, answer_stamp, end_stamp, duration, billsec,
 		                                        hangup_cause, hangup_cause_q850, accountcode, read_codec, write_codec, uuid, bleg, json)
 													VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)`,
 		res.Direction, res.CallerIDName, res.CallerIDNumber, res.CalledDestinationNumber, res.DestinationNumber, res.Context,
 		res.CoreUUID, res.Switchname, res.StartStamp, res.AnswerStamp, res.EndStamp, res.Duration, res.Billsec,
-		res.HangupCause, res.HangupCauseQ850, res.Accountcode, res.ReadCodec, res.WriteCodec, res.AlegUUID, res.BlegUUID)
+		res.HangupCause, res.HangupCauseQ850, res.Accountcode, res.ReadCodec, res.WriteCodec, res.AlegUUID, res.BlegUUID, json.String())
 
 	if qerr != nil {
-		log.Println(err)
+		w.WriteHeader(500)
+                w.Write([]byte("500 - " + http.StatusText(503) + "\nDB Query failed!"))
+		log.Print("DB Query Failed: ")
+		log.Println(qerr)
+		//log.Println(qerr.Error())
+		//if qerr, ok := qerr.(*pq.Error); ok {
+		//	fmt.Println("pq error:", qerr.Code.Name())
+		//}
 	}
+
+	//log.Println(row)
 
 	if err == nil {
 		w.WriteHeader(200)
 		w.Write([]byte("200 - " + http.StatusText(200)))
+		log.Println("Success, Inserted: " + res.CallerIDName + " -> " + res.DestinationNumber + " Accountcode: " + res.Accountcode + " Billsec: " + res.Billsec)
 	} else {
 		w.WriteHeader(503)
 		w.Write([]byte("503 - " + http.StatusText(503)))
+		log.Println("Failed")
 	}
 }
 
