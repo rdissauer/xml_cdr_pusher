b4f33a542dfc3186605c2790d2b127e418772b1a
rdissauer <roman@dissauer.net>
2016-07-14 10:58:11 +0200

    fixed error handling, fixed db insertion of null values

--- xml_cdr_pusher.go
+++ xml_cdr_pusher.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"strconv"
 	xj "github.com/basgys/goxml2json"
 
 	_ "github.com/lib/pq"
@@ -19,7 +20,7 @@ func (t *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	type Result struct {
 		XMLName                 xml.Name `xml:"cdr"`
-		Direction               string   `xml:"variables>direction"`
+		Direction               string   `xml:"variables>call_direction"`
 		CallerIDName            string   `xml:"variables>caller_id_name"`
 		CallerIDNumber          string   `xml:"variables>caller_id_number"`
 		CalledDestinationNumber string   `xml:"callflow>caller_profile>destination_number"`
@@ -30,10 +31,10 @@ func (t *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		StartStamp              string   `xml:"variables>start_stamp"`
 		AnswerStamp             string   `xml:"variables>answer_stamp"`
 		EndStamp                string   `xml:"variables>end_stamp"`
-		Duration                string   `xml:"variables>duration"`
-		Billsec                 string   `xml:"variables>billsec"`
+		Duration                int   `xml:"variables>duration"`
+		Billsec                 int   `xml:"variables>billsec"`
 		HangupCause             string   `xml:"variables>hangup_cause"`
-		HangupCauseQ850         string   `xml:"variables>hangup_cause_q850"`
+		HangupCauseQ850         int   `xml:"variables>hangup_cause_q850"`
 		Accountcode             string   `xml:"variables>accountcode"`
 		ReadCodec               string   `xml:"variables>read_codec"`
 		WriteCodec              string   `xml:"variables>write_codec"`
@@ -46,6 +47,8 @@ func (t *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	body, _ := ioutil.ReadAll(r.Body)
 	decbody, _ := url.QueryUnescape(string(body))
+	//log.Println(decbody)
+	//decbody, _ = url.QueryUnescape(decbody)
 	//log.Println(decbody)
 	xmlraw := decbody	
 	xmlreader := strings.NewReader(xmlraw)
@@ -64,9 +67,23 @@ func (t *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	err := xml.Unmarshal([]byte(decbody), res)
 
-	res.StartStamp, _ = url.QueryUnescape(res.StartStamp)
-	res.AnswerStamp, _ = url.QueryUnescape(res.AnswerStamp)
-	res.EndStamp, _ = url.QueryUnescape(res.EndStamp)
+	if err != nil {
+		w.WriteHeader(503)
+		w.Write([]byte("503 - " + http.StatusText(503)))
+		log.Print("Parsing XML failed: ")
+		log.Println(err);
+		return
+	}
+
+	var StartStamp, AnswerStamp, EndStamp sql.NullString
+
+	StartStamp.String, _ = url.QueryUnescape(res.StartStamp)
+	AnswerStamp.String, _ = url.QueryUnescape(res.AnswerStamp)
+	EndStamp.String, _ = url.QueryUnescape(res.EndStamp)
+
+	if len(StartStamp.String) == 0 {StartStamp.Valid = false } else {StartStamp.Valid = true }
+	if len(AnswerStamp.String) == 0 {AnswerStamp.Valid = false } else {AnswerStamp.Valid = true }
+	if len(EndStamp.String) == 0 {EndStamp.Valid = false } else {EndStamp.Valid = true }
 
 	//log.Println(err)
 	//log.Println(*res)
@@ -85,7 +102,7 @@ func (t *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		                                        hangup_cause, hangup_cause_q850, accountcode, read_codec, write_codec, uuid, bleg, json)
 													VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)`,
 		res.Direction, res.CallerIDName, res.CallerIDNumber, res.CalledDestinationNumber, res.DestinationNumber, res.Context,
-		res.CoreUUID, res.Switchname, res.StartStamp, res.AnswerStamp, res.EndStamp, res.Duration, res.Billsec,
+		res.CoreUUID, res.Switchname, StartStamp, AnswerStamp, EndStamp, res.Duration, res.Billsec,
 		res.HangupCause, res.HangupCauseQ850, res.Accountcode, res.ReadCodec, res.WriteCodec, res.AlegUUID, res.BlegUUID, json.String())
 
 	if qerr != nil {
@@ -93,6 +110,7 @@ func (t *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
                 w.Write([]byte("500 - " + http.StatusText(503) + "\nDB Query failed!"))
 		log.Print("DB Query Failed: ")
 		log.Println(qerr)
+		return
 		//log.Println(qerr.Error())
 		//if qerr, ok := qerr.(*pq.Error); ok {
 		//	fmt.Println("pq error:", qerr.Code.Name())
@@ -104,7 +122,7 @@ func (t *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err == nil {
 		w.WriteHeader(200)
 		w.Write([]byte("200 - " + http.StatusText(200)))
-		log.Println("Success, Inserted: " + res.CallerIDName + " -> " + res.DestinationNumber + " Accountcode: " + res.Accountcode + " Billsec: " + res.Billsec)
+		log.Println("Success, Inserted: " + res.CallerIDName + " -> " + res.DestinationNumber + " -> " + res.Direction + " Accountcode: " + res.Accountcode + " Billsec: " + strconv.Itoa(res.Billsec))
 	} else {
 		w.WriteHeader(503)
 		w.Write([]byte("503 - " + http.StatusText(503)))
